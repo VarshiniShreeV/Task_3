f9b876ddcf5fbd8c29c64be96ab122f74cdab770
root <root@DESKTOP-PN443CE.>
2025-02-24 11:04:42 +0000

    feature1 commited

--- calc.java
+++ calc.java
@@ -1,1 +1,23 @@
+import java.util.Scanner;
+                 
+public class JavaProgram
+{
+    public static void main(String args[])
+  {
+    int first, second, multiply;
+    float devide;
+    Scanner scanner = new Scanner(System.in);
 
+    System.out.print("Enter Two Numbers : ");
+    first = scanner.nextInt();
+    second = scanner.nextInt();
+
+    
+    multiply = first * second;
+    devide = (float) first / second;
+
+    
+    System.out.println("Multiplication = " + multiply);
+    System.out.println("Division = " + devide);
+  }
+}
